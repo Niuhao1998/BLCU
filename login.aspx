2770de73f5b0512bb06b11e874242d8b865f3000
Niuhao1998 <409314105@qq.com>
2019-06-09 16:16:09 +0800

    Add files via upload

---
+++ login.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="login" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>登陆</title>
+      <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+        
+    <link rel="stylesheet" href="../css/normalize.css"/>
+	<link rel="stylesheet" href="../css/common.css"/>
+	<link rel="stylesheet" href="../css/main.css"/>
+    <link href="../fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+<body>
+    <header>
+    <div class="logo">
+			<img src="../pic/logo.jpg" alt="北语信科logo" class="logo-img"/>
+			<h1 class="vertical-center">学院共享资源管理系统</h1>
+		</div>
+        </header>
+    	<div class="login-container">
+		<section class="img-display vertical-center">
+			<img src="../pic/library.png" alt=""/>
+			<img src="../pic/books1.png" alt=""/>
+			<img src="../pic/seat.png" alt=""/>
+			<img src="../pic/computer.png" alt=""/>
+		</section>
+		<section class="login vertical-center">            
+			<form action="login.aspx" name="mylogin" method="post" id="mylogin" runat="server">
+                <div><select class="form-control" style="width:313px;" name="choose">
+                    <option value="user" >用户端</option>
+                    <option value="admin">管理员端</option>
+                     </select></div>
+                 <div><span>学号：</span><input type="text" id="login_num" maxlength="12" name="id"/></div>
+				<div><span>密码：</span><input type="password" id="login_pwd" name="password"/></div>
+				<button class="login-btn"  onclick="document.mylogin.submit()" >Login</button>
+			</form>
+		</section>
+	</div>  
+</body>
+</html>
---
+++ login.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+public partial class login : System.Web.UI.Page
+{
+    public string Conn = "Data Source=DESKTOP-C2ETVMC;Initial Catalog=ClassroomRent;Integrated Security=True";
+    public string id = "";
+    public string pass = "";
+    public string Reinput_text = "";
+    public string choose = "";
+    public bool flag = false;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            Unnamed1_Click(sender, e);
+        }
+    }
+
+
+    protected void Unnamed1_Click(object sender, EventArgs e)
+    {
+        if (Request.Params["id"] != null)
+        {
+            id = Request.Params["id"].ToString();
+        }
+        if (Request.Params["password"] != null)
+        {
+            pass = Request.Params["password"].ToString();
+        }
+        if (Request.Params["choose"] != null)
+        {
+            choose = Request.Params["choose"].ToString();
+        }
+        if (id == "" || pass == "")
+        {
+            if (flag == false)
+            {
+                flag = true;
+            }
+            else
+            {
+                Response.Write("<script>alert('用户名或密码不能为空')</script>");
+            }
+        }
+        else
+        {
+            if (choose == "user")
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = Conn;
+                string sql = "select * from users where userID= '" + id + "' and password='" + pass + "'";
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        conn.Close();
+                        Session["userid"] = id;
+                        Session["password"] = pass;
+                        //重定向
+                        Response.Redirect("Users/reservation.aspx");
+
+                        //Server.Transfer();
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('用户名或密码错误')</script>");
+                    }
+                }
+                catch
+                {
+                    Response.Write("wrong!");
+                }
+            }
+            else
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = Conn;
+                string sql = "select * from Admin where ID= '" + id + "' and 密码='" + pass + "'";
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        conn.Close();
+                        Session["userid"] = id;
+                        Session["password"] = pass;
+                        //重定向
+                        Response.Redirect("Admin/reservation.aspx");
+
+                        //Server.Transfer();
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('用户名或密码错误')</script>");
+                    }
+                }
+                catch
+                {
+                    Response.Write("wrong!");
+                }
+            }
+        }
+    }
+}
